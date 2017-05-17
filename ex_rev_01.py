cd66f944e425915fd8b4291b3582504b7ad04ec1
Anderson Abreu do Nascimento <anderson.a.nascimento@gmail.com>
2017-05-17 14:39:21 -0400

    Aula em 17-05-2017 (Data Science).

---
+++ ex_rev_01.py
@@ -0,0 +1,24 @@
+
+def maiorElem(l=[]):
+    return max(l)
+
+def menorElem(l=[]):
+    return min(l)
+
+def somatorio(l=[]):
+    return sum(l)
+
+def frequencia(x, l=[]):
+    return l.count(x)
+
+def inverte(l=[]):
+    l.reverse()
+    return l
+
+l = [4, -2, 5, 0.9, 1, 5, 5]
+
+print maiorElem(l)
+print menorElem(l)
+print somatorio(l)
+print frequencia(5, l)
+print inverte(l)
