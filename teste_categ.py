b9f48d2b9f600ec86dc8b910305dee327b528b11
AndersonANascimento <anderson.a.nascimento@gmail.com>
2023-07-26 09:24:30 -0400

    Ajuste para o Python 3

--- teste_categ.py
+++ teste_categ.py
@@ -3,19 +3,19 @@ import pandas as pd
 import pydataset as pds
 
 titanic = pds.data('titanic')
-print titanic.head()
+print(titanic.head())
 
-print titanic.columns
+print(titanic.columns)
 
-print titanic['class'].describe()
+print(titanic['class'].describe())
 
-print titanic['class'].nbytes
+print(titanic['class'].nbytes)
 
-print titanic['class'][:5] == '3rd class'
+print(titanic['class'][:5] == '3rd class')
 
 titanic['class'] = titanic['class'].astype('category')
-print titanic['class'].describe()
+print(titanic['class'].describe())
 
-print titanic['class'].nbytes
+print(titanic['class'].nbytes)
 
-print titanic['class'][:5] == '3rd class'
+print(titanic['class'][:5] == '3rd class')
